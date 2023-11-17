8392b3e0b98b6cf0e02c3d78a82cd054c209f452
juliantcchu <juliantcchu@gmail.com>
2023-11-16 23:42:48 -0800

    gpt4 agent working

---
+++ GPT4_agent.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+from openai import OpenAI
+client = OpenAI()
+
+
+
+
+class GPT4_agent:
+    def __init__(self, instruction, command_docs):
+        self.instruction = instruction
+        self.command_docs = command_docs
+        self.history = [
+            {"role": "system", "content": f"Task: {instruction} \n\nAvailable Commands: \n{command_docs} \n\nInstructoin: Accomplish the Task given using the Available Commands and by observing what is presented to you. write the command between [command]. For example, [command]go east[command]"},
+        ]
+
+    def extract_command(self, text):
+        command_tag = '[command]'
+        start = text.index(command_tag) + len(command_tag)
+        code = text[start:]
+        end = code.index(command_tag)
+        code = code[:end]
+        return code
+
+
+
+    def act(self, observation):
+        self.history += [
+                {'role': 'user', 'content': observation + '\n\n What will you do next? Give me the next command. '}
+            ]
+        completion = client.chat.completions.create(
+            model="gpt-4",
+            messages=self.history
+        )
+        # print(completion)
+        response = completion.choices[0].message
+        self.history += [{'role':'assistant', 'content': response.content}]
+
+        return self.extract_command(response.content)
